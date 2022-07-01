1f1e8dfe786172410c79b35d1dcfada3f3e41b58
simonebonifazi <simonbonifraz@gmail.com>
2022-07-01 17:58:05 +0200

    milestone-1-and-propers-comments

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ questions: devo avere un limite massimo ai click dell'utente?
 //dichiaro variabili globali
 const gameStarter = document.getElementById('game-starter');
 const gameArea = document.getElementById('perimeter');
-//per contare i click/punteggio
+
+//contatore click / punteggio
 let clickCount = 0;
 
 //preparo la mia griglia
@@ -54,18 +55,21 @@ let rows = 10;
 let cols = rows;
 let totalRowsCols = rows * cols;
 gameStarter.addEventListener('click', function () {
-
+    //azzero e rielaboro la griglia al click
     gameArea.innerHTML = '';
+    //cambio la scritta al bottone 
     gameStarter.innerText = 'Ricomincia...'
 
 
     for (let i = 1; i <= totalRowsCols; i++) {
 
+        // creo cella tramite mia funzione 
         const cellElement = createCell(i);
 
-
+        //aggancio cella al click
         cellElement.addEventListener('click', function () {
 
+            //massimo un click per cella
             if (cellElement.classList.contains('clicked')) return;
 
             //incremento  al click e verifico
@@ -78,13 +82,16 @@ gameStarter.addEventListener('click', function () {
             cellElement.classList.add('clicked');
 
         })
-        //contatore click 
 
-
+        //stampo gliglie nel loro contenitore
         gameArea.appendChild(cellElement);
     }
 })
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati (delle bombe) - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 // La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// # MILESTONE 2
+// Facciamo in modo di generare 16 numeri casuali(tutti diversi) compresi tra 1 e il massimo di caselle disponibili.
+Generiamoli e stampiamo in console per essere certi che siano corretti
+*/
